5f74d10929d642717ab029e213e67ac8fe7ce9cf
Ylevo <4r2qobaf@anonaddy.me>
2023-02-22 05:27:34 +0100

    feat: added contact form with phpmailer

---
+++ src/Controllers/AboutController.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Core\Controller;
+use App\Models\CommentModel;
+use App\Models\PostModel;
+use App\Services\CommentService;
+use App\Services\PostService;
+
+class AboutController extends Controller
+{
+    public function tellAboutMe()
+    {
+        $this->render('layouts/about.html.twig');
+    }
+}
---
+++ src/Controllers/ContactSubmitController.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Core\Controller;
+use App\Models\CommentModel;
+use App\Models\PostModel;
+use App\Services\CommentService;
+use App\Services\MailerService;
+use App\Services\PostService;
+
+class ContactSubmitController extends Controller
+{
+    public function contactSubmit()
+    {
+        if (!isset($_POST['name'], $_POST['email'], $_POST['message'])) {
+            $this->badRequest();
+        }
+
+        (new MailerService($this->session))->sendEmail($_POST);
+
+        $this->render('layouts/contact.html.twig');
+    }
+}
